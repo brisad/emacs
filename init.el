cbb8b5a79527d4c1a037843a8497c637a05600dc
Michael Brennan <brennan.brisad@gmail.com>
2015-11-18 17:10:43 +0100

    Disable ido use filename at point

--- init.el
+++ init.el
@@ -193,7 +193,7 @@
 ;; ido-mode ;;
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
-(setq ido-use-filename-at-point 'guess)
+(setq ido-use-filename-at-point nil)
 (setq ido-create-new-buffer 'always)
 (ido-mode t)
 
