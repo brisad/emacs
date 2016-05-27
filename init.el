481077aa91327f55bfda15c93aaa1ae7737eae46
Michael Brennan <brennan.brisad@gmail.com>
2016-05-27 15:39:33 +0200

    Enable paredit mode

--- init.el
+++ init.el
@@ -130,6 +130,15 @@
 ;; Ag ;;
 (setq ag-highlight-search t)
 
+;; Paredit ;;
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+
 ;; imenu ;;
 
 ;; Start imenu if possible
